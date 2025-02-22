22031e39f3b7e7267ced296c82459196aaf9c879
B1udger <iliqnstefanovza@gmail.com>
2025-02-22 12:20:22 +0200

    Initial commit for SkyRush backend

---
+++ src/main/java/com/example/skyrush/SkyRushApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyRushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyrush/SkyRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
